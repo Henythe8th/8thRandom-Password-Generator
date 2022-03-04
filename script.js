8f7982f0eaaf4b7339ea1a0fdd5bffda79049180
Henythe8th <h.velascoghs@gmail.com>
2022-03-04 13:19:59 -0600

    added queries

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
 //Program to generate password
+var password = document.getElementById("password");
 
+    function genPassword(){
+        var allUserchars =[]
+        var numberChars = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+var uppercaseChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercaseChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var specialChars = [ '!', '@', '#', '$', '%', '^', '&', '*' ];
+        var howManychars =prompt("How long do you want your password to be?")
+        
+
+        var wantsNumber = confirm("Do you want numbers?")
+        var wantsCapital = confirm("Do you want uppercase letters?")
+        var wantsSpecial = confirm("Do you want special characters?")
+        var wantsLowercase = confirm("Do you want lowercase letters?")
+
+        if (wantsCapital){allUserchars = allUserchars.concat(uppercaseChars)}
+        if (wantsLowercase){allUserchars = allUserchars.concat(lowercaseChars)}
+        if (wantsNumber){allUserchars = allUserchars.concat(numberChars)}
+        if (wantsSpecial){allUserchars = allUserchars.concat(specialChars)}
+        console.log(allUserchars)    
+        var passwordLength = 12;
+        var password ="";
+
+        for (var i =0; i = passwordLength; i++){
+            var random = Math.floor(Math.random() * chars.length);
+            password += chars.substring(random, random +1)
+        }
+    }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = genPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
